158c178319a8962f2a03fc12998dcc5d306b553b
usama <usamamuhammad231@gmail.com>
2020-08-27 16:14:01 +0500

    commit first react app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App({ name, age }) {
+    return (
+        <div>
+            <div className="App">
+                Hello <strong>{name}</strong> Here From RWP
+            </div>
+            <div>
+                <ol className="ol">
+                    <li>Usama</li>
+                    <li>Umair</li>
+                    <li>Asad</li>
+                </ol>
+                <ul className="ul">
+                    <li>Rawalpindi</li>
+                    <li>Lahore</li>
+                    <li>Karachi</li>
+                </ul>
+            </div>
+            <div className="App">
+                <p>
+                    My name is <strong>Usama</strong> and Age is{" "}
+                    {(age - 10) * 2}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
